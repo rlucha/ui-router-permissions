551bb92fe650b7ce6a6a432aec920e04a6986e78
Roberto Lucha <rlucha@gmail.com>
2015-01-21 18:19:53 +0100

    1st

---
+++ angular-ui-router-permissions.js
@@ -0,0 +1,71 @@
+//  MODULE _____________________________________________________________________________________________________________
+/*    
+    @ngdoc              module
+    @name               ui.router.permissions
+
+    @description        Holds user permissions by feature and intercepts state transitions based on permission
+*/  
+//  SERVICE ____________________________________________________________________________________________________________
+/*    
+    @ngdoc              factory
+    @name               ui.router.permissions:uiRouterPermissionsSrv
+*/    
+//  DEFINITION _________________________________________________________________________________________________________
+   
+    function uiRouterPermissionsSrv($rootScope) {
+        
+        var _role = {},
+            _rolesCollection = {};
+
+        registerEvents();
+
+        return {
+            setRolesCollection: setRolesCollection,
+            setRole: setRole,
+            getRole: getRole
+        };
+
+        function setRolesCollection(rolesCollection) {
+            _rolesCollection = rolesCollection;
+            return this;
+        }
+
+        function setRole(role) {
+            if (_rolesCollection.hasOwnProperty(role)) {
+                _role = { 
+                    name: role,
+                    permissions: _rolesCollection[role]
+                };
+            } else {
+                throw new Error('role ' + _role + ' does not exists in rolesCollection');
+            }
+            return this;
+        }
+
+        function getRole() {
+            return _role;
+        }
+
+//  Intercept route change and check if it has the required role
+
+        function registerEvents() {
+            $rootScope.$on('$stateChangeStart', function (event, next) {
+                if(next.data.permission !== '*' && _role.permissions.indexOf(next.data.permission) === -1) {
+                    event.preventDefault();
+                    $rootScope.$broadcast('uiRouterPermissionsSrv::notAuthorized');
+                }
+            });
+        }
+    }
+
+//  MODULE ASSIGNEMENT _________________________________________________________________________________________________
+
+    angular.module('ui.router.permissions', [])
+          .factory('uiRouterPermissionsSrv', uiRouterPermissionsSrv);
+
+
+/*
+    uiRouterPermissionsSrv
+        .setRolesCollection(USER_ROLES)
+        .setRole('user');
+*/
